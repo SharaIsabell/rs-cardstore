61d3d9cf9daa46d1334458046d78f8eb0354b959
Shara Isabell <isabeloliveira425@gmail.com>
2025-09-11 14:22:18 -0300

    Criando homepage

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'frontend', 'ejs'));
 
 // Middlewares
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(express.urlencoded({ extended: true }));
 
 // Rotas
